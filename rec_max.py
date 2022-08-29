7e12633ea093e1d5d38060a655c82b3641de271a
Yogesh Tripathi <73730605+Yogesh007587@users.noreply.github.com>
2022-08-30 00:49:23 +0530

    Create rec_max.py

---
+++ rec_max.py
@@ -0,0 +1,13 @@
+def maximum(num1,num2,num3):
+    if(num1>num2):
+        if(num1>num3):
+            return num1
+        else:
+            return num3
+    else:
+        if(num2>num3):
+            return num2
+        else:
+            return num3
+m = maximum(45,66,123)
+print("The greatest number is : " +str(m))
